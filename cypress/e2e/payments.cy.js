0dad7e9bcc76664382eeef2bada0654d63789c5f
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2024-09-17 11:34:17 -0300

    Adicionando mais comentários

--- cypress/e2e/payments.cy.js
+++ cypress/e2e/payments.cy.js
@@ -168,36 +168,51 @@ describe('payments', () => {
   });
 
   it("Confirmando o envio", () => {
+    // Movendo para a página de pagamentos do administrador.
     cy.clickInFirst('a[href="/admin/payments/"]');
           
+    // Selecionando novos pedidos dos usuários. 
     cy.get('.ui > .sylius-filters > .sylius-filters__field > .field > #criteria_state').select('completed');
   
+    // Clicando no botão de filtar. 
     cy.get('*[class^="ui blue labeled icon button"]').click();
   
+    // Clicando no primeiro pedido. 
     cy.get('a[href^="/admin/orders/"]').filter((_, el) => {
       const href = Cypress.$(el).attr('href');
       return /\d+$/.test(href); 
     }).first().click();
 
-    cy.get('*[id^="sylius_shipment_ship_tracking"]').type("qwdqwd");
+    // Escrevendo um comentário. 
+    cy.get('*[id^="sylius_shipment_ship_tracking"]').type("Comentando.");
+
+    // Clicando em enviar. 
     cy.get('*[class^="ui labeled icon teal button"]').click();
 
+    // Checando se o corpo da notificação contém a mensagem esperada.   
     cy.get('body').should('contain', 'Shipment has been successfully updated.'); 
 
+    // Clicando em cancelar. 
     cy.get('*[class^="ui icon labeled tiny yellow fluid loadable button"]').click();
 
+    // Checando se o corpo da notificação contém a mensagem esperada.   
     cy.get('body').should('contain', 'Payment has been successfully refunded.');
   });
 
   it("Quando clicamos em next sucessivas vezes até está desabilitado", () => {
     cy.clickInFirst('a[href="/admin/payments/"]');
 
+    // Clicando em Next sucessivas vezes até ficar desabilitado. 
     function clickUntilDisabled() {
       cy.get('a').contains('Next').then($next => {
+
+        // Checando se o botão Next não tem como classe "desabled".
         if (!$next.parent().hasClass('disabled')) {
           cy.wrap($next).click().then(() => {
             cy.get('div').contains('Next').then($nextDisabled => {
               if ($nextDisabled.length > 0 && $nextDisabled.hasClass('disabled')) {
+
+                // Checando se o botão Next está desabilitado. 
                 cy.wrap($nextDisabled).should('have.class', 'disabled').and('have.class', 'item');
               } else {
                 clickUntilDisabled();
@@ -212,9 +227,16 @@ describe('payments', () => {
   });
 
   it("Quando checamos se o filtro de quantidade em 25 mostra 25 itens na tabela", () => {
+    // Movendo para a página de pagamentos do administrador.
     cy.clickInFirst('a[href="/admin/payments/"]');
+
+    // Clicando no fuiltro de quantidade mostradas. 
     cy.get('*[class^="ui simple fluid dropdown item"]').click();
+
+    // Escolhendo quantidade igual a 25. 
     cy.clickInFirst('a[href="/admin/payments/?limit=25"]');
+
+    // Checando se o tamanho da table é de 25 linhas. 
     cy.get('tbody').children().should('have.length', 25);
   });
 
